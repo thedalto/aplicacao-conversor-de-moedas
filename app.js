afee8dc19e6ea8d5c10a34ea0baca25ec49da87b
José Victor Capistrano Dias <zevictorrsp@gmail.com>
2022-11-28 10:07:35 -0300

    Feat
    Initial

--- app.js
+++ app.js
@@ -28,3 +28,73 @@
       - Para obter a key e fazer requests, você terá que fazer login e escolher
         o plano free. Seus dados de cartão de crédito não serão solicitados.
 */
+
+const currencyOneEl = document.querySelector('[data-js="currency-one"]')
+const currencyTwoEl = document.querySelector('[data-js="currency-two"]')
+const currencyEl = document.querySelector('[data-js="currencies-container"]');
+
+
+  const url = 'https://v6.exchangerate-api.com/v6/4a100b7cfa17f2816ee9f0aa/latest/USD'
+
+const getErrorMessage = errorType =>({
+  'unsupported-code': 'A moeda não existe em nosso banco de dados',
+  'invalid-key' : 'quando sua chave de API não é válida',
+  'inactive-account' : 'se o seu endereço de e-mail não foi confirmado',
+  'quota-reached ' : ' quando sua conta atingir o número de solicitações permitidas pelo seu plano',
+
+})[errorType] || 'Não foi possivel obter as informações'
+
+
+const fetchExchangeRate = async() => {
+  try {
+    const response = await fetch (url)
+
+    if (!response.ok){
+      throw new Error ('Sua conexão falhou. Não foi possivel onter as informações.')
+    }
+    const exchangeRateData = await response.json()
+
+    if (exchangeRateData.result === 'error'){
+      throw new Error (getErrorMessage(exchangeRateData['error-type']))
+    }
+    return exchangeRateData
+  } catch (err) {
+    console.log (typeof err.message)
+    alert (err.message)
+
+    const div = document.createElement('div')
+    const button = document.createElement('button')
+
+    div.textContent = err.message
+    div.classList.add('alert', 'alert-warning', 'alert-dimissible', 'fade', 'show')
+    div.setAttribute('role','alert')
+    button.classList.add('btn-close')
+    button.setAttribute('type','button')
+    button.setAttribute('aria-label', 'close')
+    
+    button.addEventListener('click', () => {
+    div.remove()
+    })
+    
+    div.appendChild (button)
+    currencyEl.insertAdjacentElement ('afterend', div)
+
+  }
+}
+
+const init = async () => {
+  const exchangeRateData = await fetchExchangeRate()
+
+  const options = Object.keys(exchangeRateData.conversion_rates)
+    .map(currency => `<option>${currency}</option>`)
+    .join('')
+
+  console.log(options)
+
+  currencyOneEl.innerHTML = options
+  currencyTwoEl.innerHTML = options
+
+}
+
+init()
+
